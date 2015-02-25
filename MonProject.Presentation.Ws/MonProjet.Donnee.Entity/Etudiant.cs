dee95c5b1173be6b4b2d4d7a7f28fdd8970af4a4
Jonathan <Jonthan.LT@gmail.com>
2015-02-25 22:50:20 +0100

    Création du projet

---
+++ MonProject.Presentation.Ws/MonProjet.Donnee.Entity/Etudiant.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MonProjet.Donnee.Entity
+{
+    class Etudiant
+    {
+        #region Variables membres
+        private int m_id;
+        private int m_annee_dbt;
+        private int m_annee_fin;
+        private List<Etudiant> m_List_Etud;
+        private int m_nb_etd;
+        #endregion
+
+        #region Accesseur
+        public int Id
+        {
+            get { return m_id; }
+            set { m_id = value; }
+        }
+
+        public int Annee_dbt
+        {
+            get { return m_annee_dbt; }
+            set { m_annee_dbt = value; }
+        }
+
+        public int Annee_fin
+        {
+            get { return m_annee_fin; }
+            set { m_annee_fin = value; }
+        }
+
+        internal List<Etudiant> List_Etud
+        {
+            get { return m_List_Etud; }
+            set { m_List_Etud = value; }
+        }
+
+        public int Nb_etd
+        {
+            get { return m_nb_etd; }
+            set { m_nb_etd = value; }
+        }
+        #endregion
+    }
+}
---
+++ MonProject.Presentation.Ws/MonProjet.Donnee.Entity/Promotion.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonProjet.Donnee.Entity
+{
+    public class Promotion
+    {
+        #region Variables membres
+        private int m_id;
+        private String m_Nom;
+        private String m_Prenom;
+        private DateTime m_DateNaissance;
+        #endregion
+
+        #region Accesseur
+        public int Id
+        {
+            get { return m_id; }
+            set { m_id = value; }
+        }
+
+        public String Nom
+        {
+            get { return m_Nom; }
+            set { m_Nom = value; }
+        }
+
+        public String Prenom
+        {
+            get { return m_Prenom; }
+            set { m_Prenom = value; }
+        }
+
+        public DateTime DateNaissance
+        {
+            get { return m_DateNaissance; }
+            set { m_DateNaissance = value; }
+        }
+        #endregion
+    }
+}
